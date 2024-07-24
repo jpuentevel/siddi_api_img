4a175d13c399e0eb663f618e130ee0b2a4919b2b
jpuentevel <jpuentevelasquez@gmail.com>
2024-07-23 20:30:42 -0500

    commit

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ from PIL import Image
 import os
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
+mysql = init_db(app)
 CORS(app)
 api = Api(app)
 
@@ -60,5 +62,10 @@ class imagen(Resource):
 
 api.add_resource(imagen, '/imagen')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+""" if __name__ == '__main__':
+    app.run(debug=True) """
+
+if __name__ == "__main__":
+    if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+        os.makedirs(app.config["UPLOAD_FOLDER"])
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
